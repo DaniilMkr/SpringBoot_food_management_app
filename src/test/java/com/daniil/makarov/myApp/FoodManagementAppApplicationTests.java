202a4bfa1ab60d34daf78d54bf082379736c0a9b
Daniil Makarov <daniilmakarovmk@gmail.com>
2023-05-18 19:32:09 +0200

    initial commit

---
+++ src/test/java/com/daniil/makarov/myApp/FoodManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniil.makarov.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
